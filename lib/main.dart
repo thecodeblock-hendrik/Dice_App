f4d1ffd13dcd529f3cb65478476b838bdf126a69
Hendrik Oosthuizen <thecodeblock.dev@gmail.com>
2025-09-08 21:00:59 +0200

    add widget and class

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:dice_app/gradient_container.dart';
 
 void main() {
   runApp(
-    MaterialApp(
+    const MaterialApp(
       home: Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Color.fromARGB(255, 121, 78, 222),
-                Color.fromARGB(255, 45, 7, 98),
-              ],
-              begin: AlignmentGeometry.topLeft,
-              end: AlignmentGeometry.bottomRight,
-            ),
-          ),
-          child: const Center(
-            child: Text(
-              'Hello World!',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 40,
-              ),
-            ),
-          ),
-        ),
+        body: GradientContainer(),
       ),
     ),
   );
